368709661742ecf0512bbb2358787ac476709ca7
DESKTOP-GPEK0OJ\Randomality <brainlicker1@yahoo.com>
2023-05-26 07:22:12 -0400

    Player moves and faces the mouse pointer.

--- Assets/Script/PlayerControler.cs
+++ Assets/Script/PlayerControler.cs
@@ -11,22 +11,29 @@ public class PlayerControler : MonoBehaviour
   private PlayerControls playerControls;
   private Vector2 movement;
   private Rigidbody2D rb;
+  private Animator myAnimator;
+  private SpriteRenderer mySpriteRenderer;
 
   private void Awake() {
     playerControls = new PlayerControls();
     rb = GetComponent<Rigidbody2D>();
+    myAnimator = GetComponent<Animator>();
+    mySpriteRenderer = GetComponent<SpriteRenderer>();
   }
     private void OnEnable() {
     playerControls.Enable();
    }
      private void FixedUpdate() {
          PlayerInput();
+         AdjustPlayerFaceingDirection();
   }
      private void Update() {
       Move();
     }
     private void PlayerInput(){
     movement = playerControls.Movement.Move.ReadValue<Vector2>();
+    myAnimator.SetFloat("moveX",movement.x);
+    myAnimator.SetFloat("moveY", movement.y);
     
   }
 
@@ -34,5 +41,20 @@ public class PlayerControler : MonoBehaviour
     rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     
   }
+
+  private void AdjustPlayerFaceingDirection(){
+
+      Vector3 mousePos = Input.mousePosition;
+      Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+    if(mousePos.x < playerScreenPoint.x){
+      mySpriteRenderer.flipX = true;
+
+    } else{
+
+      mySpriteRenderer.flipX = false;
+    }
+
+  }
+
    //end Of Class
 }
